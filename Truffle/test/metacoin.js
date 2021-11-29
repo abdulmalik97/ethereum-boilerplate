568f152af4b5360aece9ea8ac6552ec4e97b9320
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-11-29 13:41:56 -0500

    Update metacoin.js

--- Truffle/test/metacoin.js
+++ Truffle/test/metacoin.js
@@ -1,37 +1,37 @@
-const MetaCoin = artifacts.require("MetaCoin");
+const BDSCICoin = artifacts.require("BDSCICoin");
 
-contract('MetaCoin', (accounts) => {
-  it('should put 10000 MetaCoin in the first account', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-    const balance = await metaCoinInstance.getBalance.call(accounts[0]);
+contract('BDSCICoin', (accounts) => {
+  it('should put 1000000000000 BDSCICoin in the first account', async () => {
+    const bdsciCoinInstance = await BDSCICoin.deployed();
+    const balance = await bdsciCoinInstance.getBalance.call(accounts[0]);
 
-    assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+    assert.equal(balance.valueOf(), 1000000000000, "1000000000000 wasn't in the first account");
   });
   it('should call a function that depends on a linked library', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
-    const metaCoinBalance = (await metaCoinInstance.getBalance.call(accounts[0])).toNumber();
-    const metaCoinEthBalance = (await metaCoinInstance.getBalanceInEth.call(accounts[0])).toNumber();
+    const bdsciCoinInstance = await BDSCICoin.deployed();
+    const bdsciCoinBalance = (await bdcsiCoinInstance.getBalance.call(accounts[0])).toNumber();
+    const bdsciCoinEthBalance = (await bdsciCoinInstance.getBalanceInEth.call(accounts[0])).toNumber();
 
-    assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, 'Library function returned unexpected function, linkage may be broken');
+    assert.equal(bdsciCoinEthBalance, 2 * bdsciCoinBalance, 'Library function returned unexpected function, linkage may be broken');
   });
   it('should send coin correctly', async () => {
-    const metaCoinInstance = await MetaCoin.deployed();
+    const bdsciCoinInstance = await BDSCICoin.deployed();
 
     // Setup 2 accounts.
     const accountOne = accounts[0];
     const accountTwo = accounts[1];
 
     // Get initial balances of first and second account.
-    const accountOneStartingBalance = (await metaCoinInstance.getBalance.call(accountOne)).toNumber();
-    const accountTwoStartingBalance = (await metaCoinInstance.getBalance.call(accountTwo)).toNumber();
+    const accountOneStartingBalance = (await bdsciCoinInstance.getBalance.call(accountOne)).toNumber();
+    const accountTwoStartingBalance = (await bdsciCoinInstance.getBalance.call(accountTwo)).toNumber();
 
     // Make transaction from first account to second.
     const amount = 10;
-    await metaCoinInstance.sendCoin(accountTwo, amount, { from: accountOne });
+    await bdsciCoinInstance.sendCoin(accountTwo, amount, { from: accountOne });
 
     // Get balances of first and second account after the transactions.
-    const accountOneEndingBalance = (await metaCoinInstance.getBalance.call(accountOne)).toNumber();
-    const accountTwoEndingBalance = (await metaCoinInstance.getBalance.call(accountTwo)).toNumber();
+    const accountOneEndingBalance = (await bdsciCoinInstance.getBalance.call(accountOne)).toNumber();
+    const accountTwoEndingBalance = (await bdsciCoinInstance.getBalance.call(accountTwo)).toNumber();
 
 
     assert.equal(accountOneEndingBalance, accountOneStartingBalance - amount, "Amount wasn't correctly taken from the sender");
