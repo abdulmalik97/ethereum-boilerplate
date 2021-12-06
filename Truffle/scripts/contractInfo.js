e027381f411d09552ec2143eab6e888ae30ed111
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-12-06 17:40:14 -0500

    Update contractInfo.js

--- Truffle/scripts/contractInfo.js
+++ Truffle/scripts/contractInfo.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-fs.copyFile("build/contracts/BDSCI1.json", "../src/contracts/contractInfo.json", (err) => {
+fs.copyFile("build/contracts/BlackDiamondSCInc1.json", "../src/contracts/contractInfo.json", (err) => {
   if (err) throw err;
   console.log("Your contract's ABI was copied to the frontend");
 });
